978a863968b4071dce6413eac3bbee2e2bbad25f
inubayuaji <inubayuaji@gmail.com>
2021-08-06 17:38:11 +0700

    chart dash board

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tagihan;
 use Illuminate\Http\Request;
 
 class DashController extends Controller
 {
     public function index()
     {
-        return view('dash');
+        $pendataan = [
+            (int)Tagihan::where('status', 1)
+                ->where('bulan', (int)date('m'))
+                ->count(),
+            (int)Tagihan::where('status', '!=', 1)
+                ->where('bulan', (int)date('m'))
+                ->count()
+        ];
+
+        $pembayaran = [
+            // sebagian
+            (int)Tagihan::where('status', 3)
+                ->where('bulan', (int)date('m'))
+                ->sum('bayar'),
+
+            // lunas
+            (int)Tagihan::where('status', 4)
+                ->where('bulan', (int)date('m'))
+                ->sum('bayar'),
+
+            // belum terbayar
+            (int)Tagihan::where('status', 2)
+                ->orWhere('status', 3)
+                ->where('bulan', (int)date('m'))
+                ->sum(\DB::raw('total - bayar'))
+        ];
+
+        return view('dash', [
+            'pendataan' => $pendataan,
+            'pembayaran' => $pembayaran,
+        ]);
     }
 }
--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -1,3 +1,90 @@
 @extends('adminlte::page')
 
 @section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Pendataan bulan ini</h3>
+                </div>
+                <div class="card-body">
+                    <div class="chart">
+                        <canvas id="data-pendataan"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Pembayaran bulan ini</h3>
+                </div>
+                <div class="card-body">
+                    <div class="chart">
+                        <canvas id="data-pembayaran"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" herf="{{ asset('vendor/chart.js/Chart.min.css') }}">
+@stop
+
+@section('js')
+    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+
+    <script>
+        new Chart($('#data-pendataan'), {
+            type: 'pie',
+            data: {
+                labels: ['Belum', 'Sudah'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: {{ json_encode($pendataan)}},
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
+
+        new Chart($('#data-pembayaran'), {
+            type: 'pie',
+            data: {
+                labels: ['Sebagian', 'Lunas', 'Belum'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: {!! json_encode($pembayaran) !!},
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
+    </script>
+@stop
