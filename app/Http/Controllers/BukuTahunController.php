29abde25af9a343eaab2e0e56a667d258fea98d2
inubayuaji <inubayuaji@gmail.com>
2021-10-10 10:05:07 +0700

    nama pelanggan di buku tahun

--- app/Http/Controllers/BukuTahunController.php
+++ app/Http/Controllers/BukuTahunController.php
@@ -126,7 +126,7 @@ class BukuTahunController extends Controller
                     return $model->petugas->nama ?? null;
                 })
                 ->editColumn('pelanggan_id', function($model){
-                    return $model->pelanggan->no ?? null;
+                    return $model->pelanggan->nama ?? null;
                 })
                 ->addColumn('action', function($model){
                     return $this->rowTagihanActions($model);
